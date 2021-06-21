f815fcf20beab25ce05f9e4605e44be4a2855182
reyhane <reeyhane.shahrokh@gmail.com>
2021-06-20 18:13:36 -0800

    register page cs

---
+++ register.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProject
+{
+    /// <summary>
+    /// Interaction logic for register.xaml
+    /// </summary>
+    public partial class register : Window
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+    }
+}
