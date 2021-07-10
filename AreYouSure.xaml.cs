a8d7c6bc1aa0f74591c586946e9c1833796465f6
reyhane <reeyhane.shahrokh@gmail.com>
2021-07-09 22:41:00 -0800

    Are you sure page

---
+++ AreYouSure.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProject
+{
+    public partial class AreYouSure : Window
+    {
+        string name;
+        string who;
+        public AreYouSure(string name,string who)
+        {
+            this.name = name;
+            this.who = who;
+            InitializeComponent();
+        }
+
+        private void Sure_Click(object sender, RoutedEventArgs e)
+        {
+            if (who == "employee")
+            {
+                Admin.DeleteEmployee(name);
+            }
+            else if (who == "book")
+            {
+                Admin.DeleteBook(name);
+            }
+            this.Close();
+        }
+
+        private void Cancle_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
