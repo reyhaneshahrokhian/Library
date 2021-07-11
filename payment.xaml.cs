405837809c9e069cb514e77aed381d73e0dd7130
reyhane <reeyhane.shahrokh@gmail.com>
2021-07-10 18:01:13 -0800

    final edit of payment page

--- payment.xaml.cs
+++ payment.xaml.cs
@@ -28,38 +28,47 @@ namespace WpfProject
             this.phone = phone;
             this.password = password;
             this.picture = picture;
+
             InitializeComponent();
         }
         private void PayEnd_Click(object sender, RoutedEventArgs e)
         {
             //payment for registering
 
-            string CreditNum = Account1.Text + Account2.Text + Account3.Text + Account4.Text;
-            if (!Pay.CheckCreditNumber(CreditNum))
-                AccountNumberBlock.Text = "The account number is invalid";
-
-            else
-                AccountNumberBlock.Text = " ";
-
-            if (!Pay.CheckCVV(CvvBox.Text))
-                cvvBlock.Text = "The CVV is invalid";
-
-            else
-                cvvBlock.Text = " ";
-
-            if (!Pay.CheckExpireDate(int.Parse(YearBox.Text), int.Parse(MonthBox.Text)))
-                DateBlock.Text = "The expire date has pass over";
-
-            else
-                DateBlock.Text = " ";
-
-            if (Pay.CheckCreditNumber(CreditNum) && Pay.CheckCVV(CvvBox.Text) && Pay.CheckExpireDate(int.Parse(YearBox.Text), int.Parse(MonthBox.Text)))
+            try
             {
-                User user = new User(name, email, phone, password, picture);
-                MainWindow m = new MainWindow();
-                m.Show();
-                this.Close();
+                string CreditNum = Account1.Text + Account2.Text + Account3.Text + Account4.Text;
+                if (!Pay.CheckCreditNumber(CreditNum))
+                    AccountNumberBlock.Text = "The account number is invalid";
+
+                else
+                    AccountNumberBlock.Text = " ";
+
+                if (!Pay.CheckCVV(CvvBox.Text))
+                    cvvBlock.Text = "The CVV is invalid";
+
+                else
+                    cvvBlock.Text = " ";
+
+                if (!Pay.CheckExpireDate(int.Parse(YearBox.Text), int.Parse(MonthBox.Text)))
+                    DateBlock.Text = "The expire date has pass over";
+
+                else
+                    DateBlock.Text = " ";
+
+                if (Pay.CheckCreditNumber(CreditNum) && Pay.CheckCVV(CvvBox.Text) && Pay.CheckExpireDate(int.Parse(YearBox.Text), int.Parse(MonthBox.Text)))
+                {
+                    User user = new User(name, email, phone, password, picture);
+                    MainWindow m = new MainWindow();
+                    m.Show();
+                    this.Close();
+                }
             }
+            catch { }
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
